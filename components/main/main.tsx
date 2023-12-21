755263e3091706216eaba717968aee7ddc3da981
oxy-Op <mdrehaan7766@gmail.com>
2023-12-21 22:24:39 +0530

    added drawer and header

---
+++ components/main/main.tsx
@@ -0,0 +1,14 @@
+import ConnectButton from "../modals/components/authenticate";
+
+const Main = () => {
+  return (
+    <main
+      className="flex flex-col items-center justify-center 
+      w-full h-[calc(100dvh-68.8px)]"
+    >
+      <h1 className="text-4xl">Welcome to Santa Drops</h1>
+    </main>
+  );
+};
+
+export default Main;
