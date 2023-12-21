9e4931e748e3e57fdc4787b9c43821e32e8e276b
oxy-Op <mdrehaan7766@gmail.com>
2023-12-22 01:34:04 +0530

    change structure

--- components/modals/components/authenticate.tsx
+++ components/modals/components/authenticate.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  useDisconnect,
-  useWeb3Modal,
-  useWeb3ModalAccount,
-} from "@web3modal/ethers/react";
+import { useWeb3Modal, useWeb3ModalAccount } from "@web3modal/ethers/react";
 import { Button } from "../../ui/button";
 import { useModal } from "@/hooks/use-modal";
 
@@ -22,13 +18,13 @@ export default function ConnectButton() {
   };
 
   return (
-    <div className="flex space-x-3">
+    <div className="flex">
       {!isConnected ? (
         <Button
           onClick={handleConnect}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-auto"
         >
-          Connect
+          Connect Your Wallet
         </Button>
       ) : (
         <Button onClick={() => onOpen("profileDrawer")}>Account</Button>
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import ProfileDrawer from "./modals/profile-drawer";
+import ProfileDrawer from "../modals/profile-drawer";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
