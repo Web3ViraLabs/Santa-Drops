089cdd274802001e6247e55063c8d6e9e9003a12
HussyDaddy <flybizit@gmail.com>
2023-12-21 23:19:16 +0530

    Navbar, footer, Configs, and much more done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
-import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Web3ModalProvider } from "@/context/web3-modal";
+import { ThemeProvider } from '@/components/theme-provider';
+import { Web3ModalProvider } from '@/context/web3-modal';
+import Navbar from '@/components/Navbar';
+import { cn, constructMetadata } from '@/lib/utils';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Footer from '@/components/Footer';
 
-const inter = Poppins({
-  subsets: ["latin"],
-  weight: "400",
-});
+import { Toaster } from 'sonner';
 
-export const metadata: Metadata = {
-  title: "Santa Drop",
-  description: "",
-};
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
@@ -20,15 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange
-        >
-          <Web3ModalProvider>{children}</Web3ModalProvider>
-        </ThemeProvider>
+    <html lang="en" className="h-full">
+      <body
+        className={cn('relative h-full font-sans antialiased', inter.className)}
+      >
+        <main className="relative flex flex-col min-h-screen">
+          <Navbar />
+          <div className="flex-grow flex-1">{children}</div>
+          <Footer />
+        </main>
+
+        <Toaster position="top-center" richColors />
       </body>
     </html>
   );
---
+++ components/Navbar.tsx
@@ -0,0 +1,89 @@
+import Link from 'next/link';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { Icons } from './Icons';
+import NavItems from './NavItems';
+import { Button, buttonVariants } from '@/components/ui/button';
+import { cookies } from 'next/headers';
+import MobileNav from './MobileNav';
+
+const Navbar = async () => {
+  const nextCookies = cookies();
+
+  /* const { user } = await getServerSideUser(nextCookies) */
+
+  return (
+    <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="relative bg-white">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              <MobileNav />
+
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
+
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {/*  {user ? null : (  */}
+                  <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  <Link
+                    href="#"
+                    className={buttonVariants({
+                      variant: 'ghost',
+                    })}
+                  >
+                    Participated Giveaways
+                  </Link>
+                  {/*  )} */}
+
+                  {/* {user ? null : ( */}
+                  <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  {/*  )} */}
+
+                  {/*  {user ? ( 
+                        <UserAccountNav user={user} />
+                      ) : ( */}
+                  <Link
+                    href="#"
+                    className={buttonVariants({
+                      variant: 'ghost',
+                    })}
+                  >
+                    Manage Giveaway's
+                  </Link>
+                  {/*    )} */}
+
+                  {/*  {user ? ( *
+                  <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                     ) : null} */}
+
+                  {/* {user ? null : ( */}
+                  <div className="flex lg:ml-6">
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  </div>
+                  {/*   )} */}
+
+                  {/* TODO: Connect this with Wallet Sheet component or give it a Complete route*/}
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Link href="#" className={buttonVariants()}>
+                      Connect Wallet
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
