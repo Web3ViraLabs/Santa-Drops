856170e1bbf7448a5d89626fab887fea955548ac
HussyDaddy <flybizit@gmail.com>
2023-12-23 02:21:33 +0530

    Allblockchain component Ui created

---
+++ config/supportedblockchain-config.tsx
@@ -0,0 +1,37 @@
+export const SUPPORTEDBLOCKCHAIN_CONFIG = [
+  {
+    name: 'Ethereum',
+    image: '/eth.png',
+    status: 'active',
+    description:
+      'Get your assets delivered.',
+  },
+  {
+    name: 'Bitcoin',
+    image: '/btc.png',
+    status: 'active',
+    description:
+      'Get your assets delivered.',
+  },
+  {
+    name: 'Solana',
+    image: '/sol.png',
+    status: 'active',
+    description:
+      "We've pledged 1% of sales.",
+  },
+  {
+    name: 'Polygon',
+    image: '/matic.png',
+    status: 'active',
+    description:
+      "We've pledged 1% of sales.",
+  },
+  {
+    name: 'Arbitrum',
+    image: '/eth.png',
+    status: 'active',
+    description:
+      "We've pledged 1% of sales.",
+  },
+];
